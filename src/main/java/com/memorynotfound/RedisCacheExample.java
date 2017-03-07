a5591029cb1e84e627768229d918325fa50fa5d2
qsjhhgz <qsjhhgz@gmail.com>
2017-03-07 09:59:09 +0800

    'files'

---
+++ src/main/java/com/memorynotfound/RedisCacheExample.java
@@ -0,0 +1,21 @@
+package com.memorynotfound;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class RedisCacheExample {
+
+    public static void main(String[] args) {
+
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
+        MusicService musicService = ctx.getBean(MusicService.class);
+
+        System.out.println("message: " + musicService.play("trumpet"));
+        System.out.println("message: " + musicService.play("trumpet"));
+        System.out.println("message: " + musicService.play("guitar"));
+        System.out.println("message: " + musicService.play("guitar"));
+
+        System.out.println("Done.");
+    }
+
+}
